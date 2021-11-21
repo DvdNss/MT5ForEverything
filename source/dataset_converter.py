6943cafb5236c9127e1162b729a8c8cb3f77e5eb
DvdNss <private.david.naisse@gmail.com>
2021-11-21 18:27:49 +0100

    added squad converter

--- source/dataset_converter.py
+++ source/dataset_converter.py
@@ -31,6 +31,7 @@ def xsum_summarization(dict: dict):
     Map function for xsum summarization task.
 
     :param dict: set of features
+
     :return: input/output list of size 2
     """
 
@@ -40,7 +41,70 @@ def xsum_summarization(dict: dict):
     return [input, output]
 
 
-def dataset_to_csv(name: str, path: str, map_function, option: str = '', **kwargs):
+def squad_converter():
+    """
+    Convert squad to a better format.
+    """
+
+    dataset = load_dataset('squad')
+    transformed_dataset = {}
+
+    # Convert dataset for each subset
+    for subset in dataset:
+
+        # Generate new dict
+        transformed_subset = {}
+
+        # Transform subset
+        for row in tqdm(dataset[subset]):
+            context = row['context']
+            question = row['question']
+            index = row['answers']['answer_start'][0]
+            answer = row['answers']['text'][0]
+
+            if row['context'] not in transformed_subset:
+                transformed_subset[f"{context}"] = []
+            else:
+                transformed_subset[f"{context}"].append({
+                    'question': question,
+                    'index': index,
+                    'answer': answer
+                })
+
+        new_subset = []
+        for key in transformed_subset.keys():
+            new_subset.append([key, transformed_subset[key]])
+
+        transformed_dataset[subset] = new_subset
+
+    return transformed_dataset
+
+
+def squad_qaqg(tab):
+    """
+    Map function for squad qa-qg task.
+
+    :param tab: set of features
+
+    :return: input/output list of size 2
+    """
+
+    context = tab[0]
+    items = tab[1]  # question, index, answer
+
+    input_ae = context
+    for answer in items:
+        question = answer['question']
+        index = answer['index']
+        answer = answer['answer']
+        # TODO: generate input/output dataset for squad here
+
+        context[index: index + len(answer['answer'])].replace(answer['answer'])
+
+    return 'ok'
+
+
+def dataset_to_csv(name: str, path: str, map_function, dataset: dict, option: str = '', **kwargs):
     """
     Convert dataset to input/output tsv file.
 
@@ -48,14 +112,18 @@ def dataset_to_csv(name: str, path: str, map_function, option: str = '', **kwarg
     :param name: dataset name
     :param option: dataset option
     :param map_function: function used to map dataset
+    :param dataset: custom dataset
     """
 
     # Load specified dataset with given options
-    dataset = load_dataset(path=name, name=option) if option != "" else load_dataset(path=name)
+    if name != 'custom':
+        dataset = load_dataset(path=name, name=option) if option != "" else load_dataset(path=name)
+    else:
+        dataset = dataset
 
     # Create output directory if needed
     try:
-        os.makedirs(path+option)
+        os.makedirs(path + option)
     except FileExistsError:
         # directory already exists
         pass
@@ -68,5 +136,8 @@ def dataset_to_csv(name: str, path: str, map_function, option: str = '', **kwarg
 
 
 if __name__ == '__main__':
-    dataset_to_csv(name='mlsum', option='fr', path='../data/mlsum/', map_function=mlsum_summarization, sep='\t')
+    dataset = squad_converter()
+    # print(dataset)
+    dataset_to_csv(name='custom', dataset=dataset, path='../data/squad-reworked/', map_function=squad_qaqg, sep='\t')
+    # dataset_to_csv(name='mlsum', option='fr', path='../data/mlsum/', map_function=mlsum_summarization, sep='\t')
     # dataset_to_csv(name='xsum', option='', path='../data/xsum/', map_function=xsum_summarization, sep='\t')

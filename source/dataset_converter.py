3f795ff4549369722b516067d649c924156c1343
DvdNss <private.david.naisse@gmail.com>
2021-11-22 12:55:52 +0100

    updated for xsum

--- source/dataset_converter.py
+++ source/dataset_converter.py
@@ -12,20 +12,6 @@ from datasets import load_dataset
 from tqdm import tqdm
 
 
-def mlsum_summarization(dict: dict):
-    """
-    Map function for mlsum summarization task.
-
-    :param dict: set of features
-    :return: input/output list of size 2
-    """
-
-    input = dict['text']
-    output = dict['summary']
-
-    return [input, output]
-
-
 def xsum_summarization(dict: dict):
     """
     Map function for xsum summarization task.
@@ -39,69 +25,6 @@ def xsum_summarization(dict: dict):
     output = dict['summary'].replace('\n', '')
 
     return [input, output]
-
-
-def squad_converter():
-    """
-    Convert squad to a better format.
-    """
-
-    dataset = load_dataset('squad')
-    transformed_dataset = {}
-
-    # Convert dataset for each subset
-    for subset in dataset:
-
-        # Generate new dict
-        transformed_subset = {}
-
-        # Transform subset
-        for row in tqdm(dataset[subset]):
-            context = row['context']
-            question = row['question']
-            index = row['answers']['answer_start'][0]
-            answer = row['answers']['text'][0]
-
-            if row['context'] not in transformed_subset:
-                transformed_subset[f"{context}"] = []
-            else:
-                transformed_subset[f"{context}"].append({
-                    'question': question,
-                    'index': index,
-                    'answer': answer
-                })
-
-        new_subset = []
-        for key in transformed_subset.keys():
-            new_subset.append([key, transformed_subset[key]])
-
-        transformed_dataset[subset] = new_subset
-
-    return transformed_dataset
-
-
-def squad_qaqg(tab):
-    """
-    Map function for squad qa-qg task.
-
-    :param tab: set of features
-
-    :return: input/output list of size 2
-    """
-
-    context = tab[0]
-    items = tab[1]  # question, index, answer
-
-    input_ae = context
-    for answer in items:
-        question = answer['question']
-        index = answer['index']
-        answer = answer['answer']
-        # TODO: generate input/output dataset for squad here
-
-        context[index: index + len(answer['answer'])].replace(answer['answer'])
-
-    return 'ok'
 
 
 def dataset_to_csv(name: str, path: str, map_function, dataset: dict = {}, size: int = -1, option: str = '', **kwargs):
@@ -138,13 +61,4 @@ def dataset_to_csv(name: str, path: str, map_function, dataset: dict = {}, size:
 
 
 if __name__ == '__main__':
-    # # SQUAD
-    # dataset = squad_converter()
-    # print(dataset)
-    # dataset_to_csv(name='custom', dataset=dataset, path='../data/squad-reworked/', map_function=squad_qaqg, sep='\t')
-
-    # # MLSUM
-    # dataset_to_csv(name='mlsum', option='fr', path='../data/mlsum/', map_function=mlsum_summarization, sep='\t')
-
-    # XSUM
     dataset_to_csv(name='xsum', option='', path='../data/xsum/', map_function=xsum_summarization, sep='\t')
